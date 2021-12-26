1c23a44f0fb3713fc85967d48cfca8156945cfe0
Denis Onder <denis_onder@protonmail.com>
2021-12-26 21:00:24 +0100

    Rendering 50 random word pairs

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData.dark(),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("WordPair Generator"),
-          ),
-          body: Center(
-            child: Text("Hello world!"),
-          ),
-        ));
+    return const MaterialApp(
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  Widget _buildList() {
+    final list = <Widget>[];
+
+    for (var i = 0; i < 50; i++) {
+      list.add(Container(
+        height: 50,
+        color: Colors.white,
+        child: Center(
+          child: Text(WordPair.random().asPascalCase),
+        ),
+      ));
+    }
+
+    return ListView(padding: const EdgeInsets.all(8), children: list);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("WordPair Generator"),
+      ),
+      body: _buildList(),
+    );
   }
 }
